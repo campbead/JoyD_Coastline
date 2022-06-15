b7ed5ca51cbe97185636c7d85f2d6300e65d2c02
Adam Campbell <campbead@gmail.com>
2022-06-16 08:46:52 +1200

    initial commit

---
+++ query_api.R
@@ -0,0 +1,14 @@
+#load libraries
+library(httr)
+
+# setup api key
+api_key <- readr::read_file("api_key.txt")
+
+
+# make request
+r <- GET("https://data.linz.govt.nz/services/query/v1/raster.json", 
+    query = list(key = api_key, layer = "51768", x = "170.78958785400164", y = "-44.52805255369485")
+)
+
+# see response text
+content(r,"text")
